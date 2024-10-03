f7b2fb250e32f999d24661679250a9bbce96297e
lebatrung <trunglehotface@gmail.com>
2024-10-03 21:33:12 +0700

    Da hoan thanh

---
+++ Lab5/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap so diem trung binh cua sinh vien:");
+        float D = float.Parse(Console.ReadLine());
+        if (0 <= D && D < 5) Console.WriteLine("Yếu");
+        else if (5 <= D && D < 7) Console.WriteLine("Tb");
+        else if (7 <= D && D < 8) Console.WriteLine("Khá");
+        else if (8 <= D && D <= 10) Console.WriteLine("Giỏi");
+        else Console.WriteLine("Nhap sai so diem!");
+    }
+}
