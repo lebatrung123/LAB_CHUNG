9cc1ac8fea078b6ea5d12537c0d88c8d3c6a37f4
lebatrung <trunglehotface@gmail.com>
2024-10-03 21:03:45 +0700

    Hoan thanh lab4

---
+++ Lab4/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+internal class Program
+{
+    public static void Main(string[] agrs)
+    {
+        const int Quydoi = 23000;
+
+        Console.WriteLine("Nhap vao so tien ma ban co");
+        int usd = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"So tien quy doi la:{Quydoi * usd}");
+
+    }
+}
+
+
