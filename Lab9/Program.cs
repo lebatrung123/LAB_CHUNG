8574fbd839d13db9d52b1ee49820a5ec78b040b1
lebatrung <trunglehotface@gmail.com>
2024-10-05 20:16:20 +0700

    đã xong lab9

---
+++ Lab9/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap vao 1 so");
+        int so = Convert.ToInt32(Console.ReadLine());
+        if (so > 0 && so < 5)
+        {
+            switch (so)
+            {
+                case 1:
+                    Console.WriteLine("*");
+                    Console.WriteLine("**");
+                    Console.WriteLine("***");
+                    Console.WriteLine("****");
+                    break;
+                case 2:
+                    Console.WriteLine("*****");
+                    Console.WriteLine("*****");
+                    Console.WriteLine("*****");
+                    Console.WriteLine("*****");
+                    break;
+                case 3:
+                    Console.WriteLine("******");
+                    Console.WriteLine("******");
+                    Console.WriteLine("******");
+                    Console.WriteLine("******");
+                    break;
+                case 4:
+                    Console.WriteLine("***");
+                    Console.WriteLine("* *");
+                    Console.WriteLine("* *");
+                    Console.WriteLine("***");
+                    break;
+                case 0:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Nhap sai khoang");
+        }
+    }
+}
