682ac4d880e445fecf0784510c704f526d1d7fd6
lebatrung <trunglehotface@gmail.com>
2024-10-06 13:55:45 +0700

    đã hoàn thành

---
+++ Lab10_Insonguyento/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int dem = 0;
+        Console.WriteLine("Danh sach cac so nguyen to nho hơn 100:");
+        for (int i = 2; i <= 100; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                if (i % j == 0)
+                {
+                    dem++;
+                }
+            }
+            if (dem == 2)
+            {
+                Console.WriteLine(i);
+            }
+            dem = 0;
+        }
+
+    }
+
+}
