61c0b0b280acd2e5b54aa644b5a3fb42068c1b77
lebatrung <trunglehotface@gmail.com>
2024-10-06 12:44:39 +0700

    đã haon thanh

---
+++ Lab7_Phuongtrinhbacnhat/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double c;
+        Console.WriteLine("Nhập a:");
+        double a = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Nhập b:");
+        double b = Convert.ToDouble(Console.ReadLine());
+        if (a != 0)
+        {
+            c = -b / a;
+            Console.WriteLine("Phương có nghiệm là:" + c);
+
+
+        }
+        else if (b == 0)
+        {
+            Console.WriteLine("Phương trình vô số nghiệm");
+        }
+        else
+        {
+            Console.WriteLine("Phương trình vô nghiệm");
+        }
+    }
+
+}
+
