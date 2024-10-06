5fa37b9262d74bf43d6a86915c4fca6e12480a13
lebatrung <trunglehotface@gmail.com>
2024-10-06 13:46:47 +0700

    đã hoan thanh

---
+++ Lab9_Tinhtienlai/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double money = 1.0;
+        int month = 1;
+        double Rate = 1.0;
+        Console.WriteLine("Nhập số tiền gửi:");
+        money = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Nhập số tháng:");
+        month = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Nhập tỉ lệ lãi suất:");
+        Rate = Convert.ToDouble(Console.ReadLine());
+        double interRate = 0;
+        for (int i = 0; i < month; i++)
+        {
+            interRate += money * (Rate / 100) / 12;
+        }
+        Console.WriteLine("Tổng số tiền lãi cho vay là:" + interRate);
+    }
+}
