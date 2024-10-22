9e02fa138c4b4495207da98bac115d4f805f1d7c
lebatrung <trunglehotface@gmail.com>
2024-10-22 20:19:52 +0700

    đã chinh sua

---
+++ LAPTRINHHUONGDOITUONG/LỚP/Lab4/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Program
+{
+
+   
+    private static void Main(string[] args)
+    {
+Weapo wepo1 =new Weapo(1);
+Weapo wepo2 =new Weapo(1,2);
+Weapo wepo3 =new Weapo();
+
+
+    }
+    
+
+    Fan fan1 =new Fan(3,true,10,"yellow");
+    Fan fan2 =new Fan(5,true,5,"blue");
+   
+}
