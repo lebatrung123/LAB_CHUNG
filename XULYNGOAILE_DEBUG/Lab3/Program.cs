00fa2307f2df3ef1636b12e8fb2f41c1c906f654
lebatrung <trunglehotface@gmail.com>
2024-11-14 11:42:59 +0700

    đã hoàn thành

---
+++ XULYNGOAILE_DEBUG/Lab3/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+      
+     try
+       {
+         List<Char> c =new List<Char>();
+        c.InsertRange(0, new Char[]{'a','b','c','d','e'});
+        int value = c[5];
+       }
+       catch (IndexOutOfRangeException e)
+       {
+        Console.WriteLine("Lỗi");
+        throw;
+       }
+        
+        
+      
+
+    }
+}
